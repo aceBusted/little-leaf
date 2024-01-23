f9c3cf6371d2361dc83e63ab730397c557b1b006
raka334 <readytogave@outlook.com>
2024-01-23 22:42:06 +0530

    conflict fix

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,18 +2,9 @@ import React from 'react'
 
 function Hero() {
   return (
-<<<<<<< HEAD
+
     <div>Hero</div>
   )
 }
 
 export default Hero
-=======
-    <div>
-      HEro
-    </div>
-  )
-}
-
-export default Hero
->>>>>>> cbb27a4dfc375267f62cd06d0b3efb97f65dc671
