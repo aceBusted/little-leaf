7f443948df29f87f97ad72124bae00cf391323b2
raka334 <readytogave@outlook.com>
2024-01-23 22:31:01 +0530

    added the card hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
+import Card from "@/components/cards";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Card />
     </div>
   );
 }
