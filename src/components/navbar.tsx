2ab4894dca142abe256db1c492dc19dd63f7c63c
raka334 <readytogave@outlook.com>
2024-01-24 08:27:50 +0530

    imported  Button ui

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 function Navbar() {
   return (
+    <>
     <div className='p-5 m-10 pr-14 text-gray-600'>Navbar</div>
-  )
+    <Button>Thios s os</Button>
+    </>  )
 }
 
 export default Navbar
