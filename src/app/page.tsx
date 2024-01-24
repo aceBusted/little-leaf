c2de367775b30083aebda30033386739975fc6f9
Simer <113010708+Simer13@users.noreply.github.com>
2024-01-24 20:42:16 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
-import Card from "@/components/cards";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Card/>
+      <Navbar />
+      
     </div>
   );
 }
+
