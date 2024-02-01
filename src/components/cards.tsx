01f28771e648f9d701824e7d539aa78d2784482e
Subah <89022048+subah2004@users.noreply.github.com>
2024-02-01 21:52:56 +0530

    msg

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,39 +1,84 @@
 import React from 'react';
 import feature1 from "@/config/feature1.json" ;
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/carousel";
 
 function Card() {
   return (
-    <div className='bg-gray-200 flex items-center justify-center h-screen'>
-      <div className='flex w-620 h-420 sm:w-320 h-661 basis-1/2 '>
-        {feature1.map((member)=>(
-          <div className="font-thin" key={member.name}>
-           <div className=" transition-all duration-700 hover:scale-110 flex basis-1/2">
-        <div className=' mx-2 bg-white rounded-md overflow-hidden shadow-md  '>
-            <div className='bg-gray-700 text-white px-6 py-4'>  
-            <h2 className='text-xl font-semibold'>{member.name}</h2>
-            </div>
-            <div className="">
-              <img className='w-310 h-210 sm:w-170 h-332' src={member.img}/>
-            </div>
-          </div>
-      </div>
-          </div>
-        ))}
+    <Carousel className="card-w-full  max-w-screen-center px-3 py-3 h-screen items-center">
+      <CarouselContent className='border-black justify-center'>
+      <div className="flex ">
+        <CarouselItem className="basis-1/3">
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img className="rounded-t-lg" src="feature2.avif"  />
+                </a>
+                <div className="p-5">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Planet Over Profit 2024</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Maximize your power efficiency to minimize pollution and contribute to a cleaner planet.</p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-700 dark:bg-green-700 dark:hover:bg-green-800 dark:focus:ring-green-800">
+                        Read more
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>           
+        </CarouselItem>
 
-        {/* This is tempalte div all the elements are renderd in one stlye no need to paste multiple times  */}
-      {/* <div className=" transition-all duration-700 hover:scale-110">
-        <div className='w-620 h-420 mx-2 bg-white rounded-md overflow-hidden shadow-md flex basis-1/2 sm:w-320 h-661 '>
-            <div className='bg-gray-700 text-white px-6 py-4'>
-            feature   <h2 className='text-xl font-semibold'>Feature 1</h2>
+        <CarouselItem className='basis-1/3'>
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img className="rounded-t-lg" src="feature2.avif"  />
+                </a>
+                <div className="p-5">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Planet Over Profit 2024</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Our sustainable solutions empower you to lessen your environmental impact and pave the way for a greener future.</p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-700 dark:bg-green-700 dark:hover:bg-green-800 dark:focus:ring-green-800">
+                        Read more
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
-            <div className="">
-              <img className='w-310 h-210 sm:w-170 h-332' src='https://plus.unsplash.com/premium_photo-1681987448179-4a93b7975018?q=80&w=310&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+        </CarouselItem>  
+        <CarouselItem className='basis-1/3'>
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img className="rounded-t-lg" src="feature2.avif"  />
+                </a>
+                <div className="p-5">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Planet Over Profit 2024</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Empower yourself with eco-conscious choices to decrease pollution and foster a sustainable world.</p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-700 dark:bg-green-700 dark:hover:bg-green-800 dark:focus:ring-green-800">
+                        Read more
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
-          </div>
-      </div> */}
-      </div>
-    </div>
+        
+        </CarouselItem>
+        </div>
+      <CarouselNext className='bg-green/200 border-green/200 z-50'/>
+      <CarouselPrevious className='bg-green/200 border-green/200 z-50'/>
+      </CarouselContent>     
+    </Carousel>
   )
 }
 
 export default Card
+
