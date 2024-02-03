4edeeb4ce09918d8ef93482c22bd1f8a941ce6f9
Subah <89022048+subah2004@users.noreply.github.com>
2024-02-04 01:20:27 +0530

    new changes in cards

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -9,25 +9,27 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import compt from "@/config/feature1.json";
+import { Button } from './ui/button';
 
  
 function Cards() {
   return (
-    <div className='flex justify-center'>
-    <Carousel className="w-full max-w-2xl">
+    <div className='flex justify-center hover:scale-105 transform transition-transform ease-in-out duration-100'>
+    <Carousel className="w-90 max-w-lg">
     <CarouselContent>
       {compt.map((member) => (
-        <CarouselItem key={member.name}>
+        <CarouselItem key={member.name} >
           <div className="p-1">
             <Card>
-            <CardHeader>
-      <CardTitle>{member.title}</CardTitle>
-      <CardDescription>{member.desc}</CardDescription>
-      </CardHeader>
-
-              <CardContent className="flex aspect-square items-center justify-center p-6">
-                <span className="text-4xl font-semibold"><img src={member.img} alt="" /></span>
-              </CardContent>
+            <CardContent className="flex w-100 h-100 items-center justify-center p-4">
+                <span className=" font-semibold"><img className="w-100 h-[400px] rounded"src={member.img} alt="" /></span>
+            </CardContent>
+            <CardContent>
+                <CardTitle className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{member.title}</CardTitle>
+                <CardDescription className="mb-3 font-normal text-gray-700 dark:text-gray-400">{member.desc}</CardDescription>
+                <Button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-700 dark:bg-green-700 dark:hover:bg-green-800 dark:focus:ring-green-800">Learn More</Button>
+            </CardContent>
+      
             </Card>
           </div>
         </CarouselItem>
