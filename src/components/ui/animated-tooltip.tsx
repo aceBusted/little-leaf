6a6c04899a5e45abfe1d71a4ae71fb26f7ed1e3d
Simer <simerseehra13@gmail.com>
2024-02-24 14:13:22 +0530

    fix thr link

--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -85,7 +85,7 @@ export const AnimatedTooltip = ({
             width={100}
             src={item.image}
             alt={item.name}
-            url={item.url}
+            
             className="object-cover !m-0 !p-0 object-top rounded-full h-14 w-14 border-2 group-hover:scale-150 group-hover:z-30 border-white  relative transition duration-500 scale-120"
           /> </a>
         </div>
